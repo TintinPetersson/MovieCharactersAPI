4f0dab8c2e6d55a7ee514a19c21735ef91204f73
Tintin Petersson <tpsson95@gmail.com>
2023-02-24 11:40:58 +0100

    Add project files.

---
+++ MovieCharactersAPI/Models/MovieCharactersDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieCharactersAPI.Models
+{
+    public class MovieCharactersDbContext: DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public MovieCharactersDbContext(DbContextOptions options):base (options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie { Id = 1, Name = "Filips Adventure"},
+                new Movie { Id = 2, Name = "Tommy's Wedding" }
+                );
+        }
+    }
+}
